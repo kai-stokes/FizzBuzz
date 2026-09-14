f28a2bb227496f1bae17750d2063ce816d8ae6c6
Kai Stokes <kai_stokes@MacBook-Pro-47.local>
2026-09-14 12:55:21 -0400

    refactore extract helper

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -1,5 +1,7 @@
 package lab1;
 
+import jdk.jshell.execution.JdiDefaultExecutionControl;
+
 /**
  * Solve the lab1.FizzBuzz challenge.
  */
@@ -7,30 +9,29 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
+        int i = 1;
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+        while (i <= 100) {
+            i = doFizzBuzz(i);
         }
     }
+
+    private static int doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(i);
+        }
+        i++;
+        return i;
+    }
 }
+
+
